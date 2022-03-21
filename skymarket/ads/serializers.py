b37eb81dafaa9a99691db450d10773fe20bf8576
lisvv <81390205+lisvv@users.noreply.github.com>
2022-03-21 20:05:53 +0300

    add author

--- skymarket/ads/serializers.py
+++ skymarket/ads/serializers.py
@@ -7,10 +7,12 @@ from ads.models import Ad, Comment
 class CommentSerializer(serializers.ModelSerializer):
     author_id = serializers.ReadOnlyField(source="author.id")
     ad_id = serializers.ReadOnlyField(source="ad.id")
+    author_first_name = serializers.ReadOnlyField(source="author.first_name")
+    author_last_name = serializers.ReadOnlyField(source="author.last_name")
 
     class Meta:
         model = Comment
-        fields = ("pk", "text", "created_at", "author_id", "ad_id")
+        fields = ("pk", "text", "created_at", "author_id", "ad_id", "author_first_name", "author_last_name")
 
 
 class AdSerializer(serializers.ModelSerializer):
