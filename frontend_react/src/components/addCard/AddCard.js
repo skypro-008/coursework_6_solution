eefed3274615d23b464aed4dd3e4cde52977cfb5
Sasha Stepanova <chernishevskie@yandex.ru>
2022-04-13 00:41:30 +0300

    refactor: deleted unneded useContext

--- frontend_react/src/components/addCard/AddCard.js
+++ frontend_react/src/components/addCard/AddCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import UserForm from "../userForm/UserForm";
 import Preloader from "../preloader/Preloader";
